db1886c5bd44d0a8497caf697d04cec5ecab106e
nobe4 <haffreinguev@gmail.com>
2016-12-08 00:10:33 +0000

    More fixes

--- 2016/5/chess.c
+++ 2016/5/chess.c
@@ -12,13 +12,13 @@
 
 // Create a MD5 hash from a string
 // inspired from http://stackoverflow.com/a/8389763/2558252
-void MD5(const char *str, int length, unsigned char *digest) {
+void MD5(const char *buffer, uint8_t length, unsigned char *digest) {
   // MD5 context
   MD5_CTX context;
 
   // Compute the MD5
   MD5_Init(&context);
-  MD5_Update(&context, str, length);
+  MD5_Update(&context, buffer, length);
 
   // Put the result in digest
   MD5_Final(digest, &context);
@@ -26,51 +26,55 @@ void MD5(const char *str, int length, unsigned char *digest) {
 
 // Create a string concatenating a base string with an integer
 // apply MD5 over it
-void generate_MD5(int i, char* buffer, unsigned char *md5){
-
-  // Base string, entry point of the challenge
-  char base[] = "uqwqemis";
-
-  // String containing a representation of i
-  char increment[12];
-
+void generate_MD5(const char* base, const uint8_t base_len, uint32_t i, char* increment, char* buffer, unsigned char *md5){
   // Convert i into a char, in the increment string
   // May be a faulty operation, as we aren't checking for any boundary ¯\_(ツ)_/¯
   sprintf(increment, "%d", i);
 
-  // Concatenate both strings into the buffer
-  snprintf(buffer, 32, "%s%s", base, increment);
+  // Reset the buffer content
+  memset(buffer, 0, 128);
 
+  // Copy the base string in the buffer
+  memcpy(buffer, base, base_len);
+
+  // Append the increment string after the base
+  memcpy(buffer + base_len, increment, strlen(increment));
+
+  // Generate the MD5
   MD5(buffer, strlen(buffer), md5);
 }
 
 int main(int argc, char **argv) {
-  // Reference of increment
+  // Base string, entry point of the challenge
+  char base[] = "uqwqemis";
+  // Don't recompute this
+  uint8_t base_len = 8;
+
+  // Current increment to be tested
   uint32_t increment = 0;
 
+  // String representation of increment
+  char increment_c[12];
+
   // Valid MD5 used for the second password
-  unsigned int count = 0;
+  uint8_t count = 0;
 
   // Blank passwords
-  // blank cells will be checked for non-existing char
   char password[] = "        ";
 
   // Counter to know when to stop searching for the first password
-  unsigned int first_password = 0;
+  uint8_t first_password = 0;
 
   // Position of the curren char, used to cast from hex, into int
   char *position_c = (char *)malloc(2);
-  int position;
+  uint8_t position;
 
   // Password char buffer, used to cast from hex
   char *password_buffer = (char *)malloc(2);
 
-  // String to MD5
-  char* string;
-
   // Buffer for the concatenation of the base string and the increment
   // Will contains the concatenation of the string and the int
-  char *buffer = (char*)malloc(32);
+  char *buffer = (char*)malloc(128);
 
   // Buffer for the md5 digest
   unsigned char* md5 = (unsigned char *)malloc(16);
@@ -85,7 +89,7 @@ int main(int argc, char **argv) {
   while (count < 8){
 
     // Generate the MD5 for the current increment
-    generate_MD5(increment, buffer, md5);
+    generate_MD5(base, base_len, increment, increment_c, buffer, md5);
 
     // Check the first 2 chars are '0'
     if (md5[0] == 0 && md5[1] == 0) {
@@ -97,7 +101,7 @@ int main(int argc, char **argv) {
         if (first_password < 8) {
 
           printf("%x", md5[2]);
-          first_password ++;
+          ++ first_password;
 
         }
 
@@ -105,7 +109,7 @@ int main(int argc, char **argv) {
         if (md5[2] < 8) {
 
           // Cast the position of the next char from the 3rd hex char
-          snprintf(position_c, 2, "%x", md5[2]);
+          sprintf(position_c, "%x", md5[2]);
           position = position_c[0] - '0';
 
           // Set only empty cells in the password string
@@ -115,12 +119,12 @@ int main(int argc, char **argv) {
             // 12 -> 01
             // ab -> 0a
             // Cast the char into the password buffer
-            snprintf(password_buffer, 2, "%x", md5[3]/16);
+            sprintf(password_buffer, "%x", md5[3]/16);
 
             // Assign the newly found char in the password string
             password[position] = password_buffer[0];
 
-            count ++;
+            ++ count;
           }
 
         }
@@ -129,7 +133,7 @@ int main(int argc, char **argv) {
 
     }
 
-    increment ++;
+    ++ increment;
 
   }
 
